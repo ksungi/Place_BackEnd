d730d086cf4480ebdefab0a8e7112cd8f7c2231c
ksungi <144586495+ksungi@users.noreply.github.com>
2023-10-13 09:02:07 +0900

    기능 테스트
    기능 테스트 겸 복습

---
+++ src/main/java/com/example/place/DTO/TestRequestBodyDTO.java
@@ -0,0 +1,9 @@
+package com.example.place.DTO;
+
+import lombok.Data;
+
+@Data
+public class TestRequestBodyDTO {
+	private String id;
+	private String message;
+}
---
+++ src/main/java/com/example/place/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.place.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	public String testService(){
+		return "Test, User Service";
+	}
+}
